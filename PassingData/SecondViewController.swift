de25b08d80350111285c8076ed330317943246a3
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-03-03 18:35:18 -0500

    Initial Commiy

---
+++ PassingData/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  PassingData
+//
+//  Created by Kritima Kukreja on 2020-03-03.
+//  Copyright © 2020 Kritima Kukreja. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
